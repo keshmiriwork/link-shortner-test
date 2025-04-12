d91c65b3e8c98fdb7562ce1b39d6b7f2645787ae
Keshmiri <keshmiri.work@gmail.com>
2025-04-12 15:11:02 +0330

    optimize

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/r/{link}', [LinkController::class, 'redirect']);
 
 
